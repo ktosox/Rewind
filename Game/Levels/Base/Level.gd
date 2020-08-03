51d22e32abd93ed31742463985e5b0359aa848e2
Ktosox <ktoso3@gmail.com>
2020-08-03 18:31:31 +0200

    Camera rework

--- Game/Levels/Base/Level.gd
+++ Game/Levels/Base/Level.gd
@@ -44,17 +44,18 @@ func end_rewind():
 	$RewindLayer/ReplayScreen.visible = false
 	$RewindLayer/ScreenRewinder.stop()
 	$CaptureRate.start()
-	GM.currentPlayer.toggle_camera()
+	$Watcher.followPlayer = true
 	$RewindLayer/ReplayScreen.texture = null
 	pass
 
 func start_rewind():
 	GM.currentProgress = 0
 	isRewinding = true
+	$Watcher.followPlayer = false
 	$RewindLayer/ReplayScreen.visible = true
 	$RewindLayer/ScreenRewinder.play("Rewind")
 	$CaptureRate.stop()
-	GM.currentPlayer.toggle_camera()
+
 	$Watcher.global_position = GM.currentPlayer.global_position
 	$Watcher.current=true
 	pass
@@ -62,7 +63,7 @@ func start_rewind():
 
 func _on_CaptureRate_timeout():
 	GM.currentProgress +=1
-	var camera = GM.currentPlayer.global_position
+	var camera = $Watcher.global_position
 	var screen = get_viewport().get_texture().get_data()
 	screen.flip_y()
 	screen.resize(640,360)
